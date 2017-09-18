c68f3fef96eeaf6a0085ee9cb78a13b31a941d8c
Ruy Garcia <rrgarciach@gmail.com>
2017-09-18 01:22:45 -0500

    Implementing arduino sketch for pedestrian access control.

--- ped-access/arduino_mega.ino
+++ ped-access/arduino_mega.ino
@@ -9,3 +9,19 @@ char keymap[Rows][Cols] = {
   {'7', '8', '9'},
   {'*', '0', '#'}
 };
+
+byte rPins[Rows] = {A6, A5, A4, A3};
+byte cPins[Cols] = {A2, A1, A0};
+
+Keypad kpd = Keypad(makeKeypad(keymap), rPins, cPins, Rows, Cols);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  char keypressed = kpd.getKey();
+  if (keypressed != NO_KEY) {
+    Serial.println(keypressed);
+  }
+}
