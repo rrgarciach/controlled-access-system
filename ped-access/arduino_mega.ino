6a641c86af68789e57937ddfa8179291fc7a7371
Ruy Garcia <rrgarciach@gmail.com>
2017-09-18 01:50:55 -0500

    WORK IN PROGRESS: arduino mega sketch for pedestrian access control.

--- ped-access/arduino_mega.ino
+++ ped-access/arduino_mega.ino
@@ -1,5 +1,11 @@
 #include "Keypad.h"
 
+// board's pinout:
+int lockRelPin = 2;
+int buzzerPin = 3;
+
+char keysPressed[4];
+
 const byte Rows = 4;
 const byte Cols = 3;
 
@@ -10,18 +16,49 @@ char keymap[Rows][Cols] = {
   {'*', '0', '#'}
 };
 
-byte rPins[Rows] = {A6, A5, A4, A3};
-byte cPins[Cols] = {A2, A1, A0};
+// Assign digital pinouts for keypad 1:
+byte rPins1[Rows] = {40, 42, 44, 46};
+byte cPins1[Cols] = {48, 50, 52};
 
-Keypad kpd = Keypad(makeKeypad(keymap), rPins, cPins, Rows, Cols);
+// Assign digital pinouts for keypad 2:
+byte rPins2[Rows] = {41, 43, 45, 47};
+byte cPins3[Cols] = {49, 51, 53};
+
+Keypad kpd1 = Keypad(makeKeypad(keymap), rPins, cPins, Rows, Cols);
+Keypad kpd2 = Keypad(makeKeypad(keymap), rPins, cPins, Rows, Cols);
 
 void setup() {
   Serial.begin(9600);
+  pinMode(lockRelPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
 }
 
 void loop() {
-  char keypressed = kpd.getKey();
-  if (keypressed != NO_KEY) {
-    Serial.println(keypressed);
+  char key = kpd.getKey();
+  if (key != NO_KEY) {
+    Serial.println(key);
+    onKeyPressed(key);
+  }
+}
+
+void onKeyPressed(char key) {
+  const index = sizeof(keysPressed);
+  keysPressed
+}
+
+void openLock() {
+  digitalWrite(buzzerPin, HIGH);
+  digitalWrite(lockRelPin, HIGH);
+  delay(3000);
+  digitalWrite(lockRelPin, LOW);
+  digitalWrite(buzzerPin, LOW);
+}
+
+void buzzForFail() {
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(lockRelPin, HIGH);
+    delay(100);
+    digitalWrite(lockRelPin, LOW);
+    delay(100);
   }
 }
